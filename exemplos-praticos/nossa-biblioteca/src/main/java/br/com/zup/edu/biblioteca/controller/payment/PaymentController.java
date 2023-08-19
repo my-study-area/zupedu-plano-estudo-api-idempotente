df38b5b1677c87faac8492c78803afdf92e5089c
Adriano Avelino <adriano-st777@hotmail.com>
2023-08-18 22:38:39 -0300

    adiciona exemplo de primary key

--- exemplos-praticos/nossa-biblioteca/src/main/java/br/com/zup/edu/biblioteca/controller/payment/PaymentController.java
+++ exemplos-praticos/nossa-biblioteca/src/main/java/br/com/zup/edu/biblioteca/controller/payment/PaymentController.java
@@ -4,15 +4,13 @@ import br.com.zup.edu.biblioteca.model.PaymentTransaction;
 import br.com.zup.edu.biblioteca.repository.PaymentTransactionRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping
@@ -24,8 +22,22 @@ public class PaymentController {
         this.repository = repository;
     }
 
-    @PostMapping("/api/payments")
-    public ResponseEntity<String> createPayment(@RequestBody @Valid PaymentRequestDTO paymentRequest, UriComponentsBuilder uriComponentsBuilder) {
+    @PostMapping("/api/payments1")
+    public ResponseEntity<String> createPaymentPrimaryKey(@RequestHeader("If-Match") UUID transactionId, @RequestBody @Valid PaymentRequestDTO paymentRequest, UriComponentsBuilder uriComponentsBuilder) {
+        boolean existTransaction = repository.existsByTransactionId(transactionId);
+        if(existTransaction) {
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED, "There is a payment for this transaction");
+        }
+        PaymentTransaction paymentTransaction = paymentRequest.toModel();
+        repository.save(paymentTransaction);
+        URI location = uriComponentsBuilder.path("/api/payments/{id}")
+                .buildAndExpand(paymentTransaction.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PostMapping("/api/payments2")
+    public ResponseEntity<String> createPaymentSecondaryKey(@RequestBody @Valid PaymentRequestDTO paymentRequest, UriComponentsBuilder uriComponentsBuilder) {
         boolean existTransaction = repository.existsByTransactionId(paymentRequest.getTransactionId());
         if(existTransaction) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "The payment has been processed");
